c6b1e2c4dcdd337ff4bf78ec4ff3ed4e3d72dee3
Mathias Kirk Bonde <mathiaskirkbonde@MacBook-Pro.local>
2022-04-09 21:29:14 +0200

    made some few graphs

--- forum-analysis.py
+++ forum-analysis.py
@@ -3,61 +3,100 @@ import numpy as np
 import json
 import pandas as pd
 import tags
+import matplotlib.pyplot as plt
 
-query = """{
-    posts(input: {
-        terms: {
-            limit: 200
-            meta: null
-        }
-        }) {
-        results {
-            _id
-            title slug pageUrl
-            postedAt
-            baseScore
-            voteCount
-            commentCount
-            meta
-            question
-            url
-            user {
-            username
-            slug
-            }
-            tags {
-            name
-            }
-        }
-    }
-}"""
+## uncomment until line 61 recreate dataset
 
-# make graphQL query and turn into dataframe
-url = 'https://forum.effectivealtruism.org/graphql'
-r = requests.post(url, json={'query': query})
-json_data = json.loads(r.text)
-df_data = json_data['data']['posts']['results']
-df = pd.DataFrame(df_data)
+# query = """{
+#     posts(input: {
+#         terms: {
+#             limit: 1000
+#             offset: %s
+#             meta: null
+#         }
+#         }) {
+#         results {
+#             _id
+#             title slug pageUrl
+#             postedAt
+#             baseScore
+#             voteCount
+#             commentCount
+#             meta
+#             question
+#             url
+#             user {
+#             username
+#             slug
+#             }
+#             tags {
+#             name
+#             }
+#         }
+#     }
+# }"""
 
-# clean up tags
-def tag_cleaner(tag_list):
-    tag_list = [tag['name'] for tag in tag_list]
-    return tag_list
-df.tags = df.tags.apply(lambda x: tag_cleaner(x))
+# # make series of graphQL query and turn into dataframe
+# posts = pd.DataFrame()
+# url = 'https://forum.effectivealtruism.org/graphql'
+# offset = 0
 
-# save to disk
-df.to_pickle('data/forum-analysis.pkl')
+# for i in range(0, 10):
+#     offset_query = query % (offset)
+#     r = requests.post(url, json={'query': offset_query})
+#     json_data = json.loads(r.text)
+#     df_data = json_data['data']['posts']['results']
+#     posts = posts.append(pd.DataFrame(df_data))
+#     offset += 1000
+
+# posts = posts.reset_index(drop=True)
+
+# # clean up tags
+# def tag_cleaner(tag_list):
+#     tag_list = [tag['name'] for tag in tag_list]
+#     return tag_list
+# posts.tags = posts.tags.apply(lambda x: tag_cleaner(x))
+
+# # save to disk
+# posts.to_pickle('data/forum-analysis.pkl')
+
+# Load from disk
+
+posts = pd.read_pickle('data/forum-analysis.pkl')
 
 # categorise post by tag category
+## tag category could for example be "global poverty"
+## Macro tag could for exmaple be "cause areas"
 
 # Check if tags have at-least one element common
 def tag_checker(x):
     tag_categories = []
-    for tag_type in tags.tag_types:
-        print(type(tag_type))
-        if (not set(tags.tag_types.get(tag_type)).isdisjoint(x)):
-            tag_categories.append(tag_type)
+
+    for tag_macro_category in tags.tag_types:
+        for tag_type in tags.tag_types[tag_macro_category]:
+            if (not set(tags.tag_types[tag_macro_category][tag_type]).isdisjoint(x)):
+                tag_categories.append(tag_type)
     return tag_categories
 
 
-df.tag_categories = df.tags.apply(lambda x: tag_checker(x))
+posts['tag_categories'] = posts.tags.apply(lambda x: tag_checker(x))
+
+
+# Get macro tags for each posts tag categories
+def macro_tag_checker(x):
+    macro_tag_categories = []
+    for tag_macro_category in tags.tag_types:
+        if (not set(tags.tag_types[tag_macro_category]).isdisjoint(x)):
+            macro_tag_categories.append(tag_macro_category)
+    return macro_tag_categories
+
+posts['macro_tag_categories'] = posts.tag_categories.apply(lambda x: macro_tag_checker(x))
+
+
+# format postedat to real dates
+
+posts.postedAt = pd.to_datetime(posts.postedAt)
+
+# save to disk
+
+posts.to_pickle('data/forum-analysis.pkl')
---
+++ plots.py
@@ -0,0 +1,109 @@
+import requests
+import numpy as np
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+posts = pd.read_pickle('data/forum-analysis.pkl')
+
+# Graph amount of posts with each tag
+
+# taken from https://towardsdatascience.com/dealing-with-list-values-in-pandas-dataframes-a177e534f173
+def to_1D(series):
+ return pd.Series([x for _list in series for x in _list])
+
+tags = to_1D(posts["tag_categories"]).value_counts().index
+posts_per_tag = to_1D(posts["tag_categories"]).value_counts().values
+
+fig, ax = plt.subplots(figsize = (14,4))
+ax.bar(tags,
+        posts_per_tag)
+ax.set_ylabel("forum posts", size = 12)
+ax.set_xlabel("tag")
+ax.set_title("Posts per forum tags", size = 14)
+fig.autofmt_xdate()
+
+plt.savefig('data/posts_per_tag.png')
+
+# Graph amount of posts with each macro tag
+
+fig, ax = plt.subplots(figsize = (14,4))
+ax.bar(to_1D(posts["macro_tag_categories"]).value_counts().index,
+        to_1D(posts["macro_tag_categories"]).value_counts().values)
+ax.set_ylabel("forum posts", size = 12)
+ax.set_xlabel("tag")
+ax.set_title("forum post macro tags", size = 14)
+fig.autofmt_xdate()
+
+plt.savefig('data/posts_per_macro_tag.png')
+
+###
+# Posts by tag by year
+###
+
+# remove posts from before 2011 (post-humous posts)
+posts = posts[~(posts['postedAt'].dt.year < 2011)]
+
+# plot posts by year, each bar with tags stacked on top of each other with a different color
+
+years = posts.postedAt.dt.year.value_counts().index
+posts_per_year = pd.DataFrame(columns=tags)
+for year in years:
+    posts_this_year = posts[posts.postedAt.dt.year == year]
+    tags_this_year = to_1D(posts_this_year["tag_categories"]).value_counts()
+    tags_this_year = tags_this_year.rename(year)
+    posts_per_year = posts_per_year.append(tags_this_year)
+
+posts_per_year = posts_per_year.fillna(0)
+posts_per_year.sort_index().plot(kind='bar', stacked=True, ylabel='Number of posts', xlabel='Year', legend='reverse', title='Posts by tag by year')
+
+plt.savefig('data/posts_per_tag_per_year.png')
+
+# plot posts by year, normalized
+
+normalized_posts_per_year = posts_per_year.div(posts_per_year.sum(axis=1), axis=0)
+
+normalized_posts_per_year.sort_index().plot(kind='bar', stacked=True, ylabel='Number of posts', xlabel='Year', figsize=(14,4), legend='reverse', title='Normalized posts by tag by year')
+
+plt.savefig('data/posts_per_tag_per_year_normalized.png')
+
+###
+# Average upvote per post
+###
+
+# taken from https://towardsdatascience.com/dealing-with-list-values-in-pandas-dataframes-a177e534f173
+def boolean_df(item_lists, unique_items):# Create empty dict
+    bool_dict = {}
+    
+    # Loop through all the tags
+    for i, item in enumerate(unique_items):
+        
+        # Apply boolean mask
+        bool_dict[item] = item_lists.apply(lambda x: item in x)
+            
+    # Return the results as a dataframe
+    return pd.DataFrame(bool_dict)
+
+unique_tags = tags
+tags_bool = boolean_df(posts["tag_categories"], unique_tags)
+
+upvotes_per_tag = pd.Series(index=unique_tags, data=0)
+
+# TODO: rewrite to list comprehension
+for i in posts.iterrows():
+    for tag in posts.loc[i[0]]['tag_categories']:
+        upvotes_per_tag[tag] += posts.loc[i[0]]['baseScore']
+
+fig, ax = plt.subplots(figsize = (14,4))
+upvotes_per_post = upvotes_per_tag / posts_per_tag
+ax.bar(upvotes_per_post.sort_values().index, upvotes_per_post.sort_values())
+ax.set_ylabel("upvotes", size = 12)
+ax.set_xlabel("tag")
+ax.set_title("Average upvote for tagged posts", size = 14)
+fig.autofmt_xdate()
+
+plt.savefig('data/upvotes_per_tag_per_post.png')
+
+plt.show()
